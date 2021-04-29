c9e707b78d0a164bb7b6811f2d85eb4d6a0b04e6
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-04-29 14:33:51 +0300

    Append value to input onClick

--- src/components/calculator/Button/Button.js
+++ src/components/calculator/Button/Button.js
@@ -6,46 +6,47 @@ class Button extends Component {
   constructor(props){
     super(props);
     this.state ={
-      value:'2'
+      value:''
     }
   }
 
   clickHandler = (e) => {
     const value = e.target.value;
-    this.setState({[value]: e.target.value})
+    this.setState({value: e.target.value })
   }
 
   render() {
+
     return (
       <div className="Button">
         <Display value={this.state.value} />
         <table>
           <tr>
-            <td><button>C</button></td>
-            <td><button>/</button></td>
-            <td><button>X</button></td>
-            <td><button>-</button></td>
+            <td><button onClick={this.clickHandler} value="Clear">C</button></td>
+            <td><button onClick={this.clickHandler} value="/">/</button></td>
+            <td><button onClick={this.clickHandler} value="X">X</button></td>
+            <td><button onClick={this.clickHandler} value="-">-</button></td>
           </tr>
           <tr>
             <td><button onClick={this.clickHandler} value="7">7</button></td>
-            <td><button value="8">8</button></td>
-            <td><button value="9">9</button></td>
-            <td rowSpan='2'><button>+</button></td>
+            <td><button onClick={this.clickHandler} value="8">8</button></td>
+            <td><button onClick={this.clickHandler} value="9">9</button></td>
+            <td rowSpan='2'><button onClick={this.clickHandler} value="+">+</button></td>
           </tr>
           <tr>
-            <td><button value="4">4</button></td>
-            <td><button value="5">5</button></td>
-            <td><button value="6">6</button></td>
+            <td><button onClick={this.clickHandler} value="4">4</button></td>
+            <td><button onClick={this.clickHandler} value="5">5</button></td>
+            <td><button onClick={this.clickHandler} value="6">6</button></td>
           </tr>
           <tr>
-            <td><button value="1">1</button></td>
-            <td><button value="2">2</button></td>
-            <td><button value="3">3</button></td>
-            <td rowSpan="2"><button>=</button></td>
+            <td><button onClick={this.clickHandler} value="1">1</button></td>
+            <td><button onClick={this.clickHandler} value="2">2</button></td>
+            <td><button onClick={this.clickHandler} value="3">3</button></td>
+            <td rowSpan="2"><button onClick={this.clickHandler} value="=">=</button></td>
           </tr>
           <tr>
-            <td colSpan="2"><button value="0">0</button></td>
-            <td><button>.</button></td>
+            <td colSpan="2"><button onClick={this.clickHandler} value="0">0</button></td>
+            <td><button onClick={this.clickHandler} value=".">.</button></td>
           </tr>
         </table>
       </div>
