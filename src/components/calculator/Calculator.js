9f59340627be4b18b4f842adef1f6a4d0113bc20
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-04-29 12:20:14 +0300

    Initial setup

---
+++ src/components/calculator/Calculator.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Display from './Display/Display';
+import Keypad from  './Keypad/Keypad';
+
+const Calculator = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Calculator;
